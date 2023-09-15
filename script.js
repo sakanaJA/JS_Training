31dc941bc01499502dd00f69884d1e3334e9a769
Haku <89026040+sakanaJA@users.noreply.github.com>
2023-09-15 22:23:56 +0900

    test

--- script.js
+++ script.js
@@ -91,7 +91,7 @@ particlesJS("particles-js", {
       },
       "bubble": {
         "distance": 400,
-        "size": 40,
+        "size": 30,
         "duration": 2,
         "opacity": 8,
         "speed": 3
